9ff8eb2e293fd846ba61b73f1613681f12d45240
DanDuncombe <fishermanduncombe@gmail.com>
2013-10-31 15:31:05 +0000

    Peat only spawnes below sea level now, not everywhere

--- mods/ores/init.lua
+++ mods/ores/init.lua
@@ -5,11 +5,11 @@ minetest.register_ore({
 	ore_type       = "scatter",
 	ore            = "ores:peat",
 	wherein        = "default:dirt",
-	clust_scarcity = 25*25*25,
+	clust_scarcity = 20*20*20,
 	clust_num_ores = 343,
 	clust_size     = 7,
 	height_min     = -31000,
-	height_max     = 64,
+	height_max     = 0,
 })
 
 
