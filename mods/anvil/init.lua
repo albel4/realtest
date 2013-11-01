ab05e6cd01b2e24e138c729a10fd5e68a06b8062
DanDuncombe <fishermanduncombe@gmail.com>
2013-11-01 21:17:24 +0000

    Restored anvils to original state

--- mods/anvil/init.lua
+++ mods/anvil/init.lua
@@ -21,7 +21,7 @@ function realtest.register_anvil_recipe(RecipeDef)
 	if recipe.level < 0 then
 		recipe.level = 0
 	end
-	if recipe.output ~= "" and recipe.item1 ~= "" and recipe.type == "forge" then
+	if recipe.output ~= "" and recipe.item1 ~= "" and (recipe.type == "forge" or recipe.type == "weld") then
 		table.insert(realtest.registered_anvil_recipes, recipe)
 	end
 end
@@ -57,13 +57,14 @@ for i, metal in ipairs(metals.list) do
 		instrument = "chisel"
 	})
 	realtest.register_anvil_recipe({
-		
+		type = "weld",
 		item1 = "metals:"..metal.."_ingot",
 		item2 = "metals:"..metal.."_ingot",
 		output = "metals:"..metal.."_doubleingot",
 		level = metals.levels[i] - 1,
 	})
 	realtest.register_anvil_recipe({
+		type = "weld",
 		item1 = "metals:"..metal.."_sheet",
 		item2 = "metals:"..metal.."_sheet",
 		output = "metals:"..metal.."_doublesheet",
@@ -188,25 +189,27 @@ for _, anvil in ipairs(anvils) do
 		groups = {oddly_breakable_by_hand=2, falling_node=1, dig_immediate=1},
 		sounds = default.node_sound_stone_defaults(),
 		can_dig = function(pos,player)
-			local meta = minetest.get_meta(pos);
+			local meta = minetest.env:get_meta(pos);
 			local inv = meta:get_inventory()
 			if inv:is_empty("src1") and inv:is_empty("src2") and inv:is_empty("hammer")
-				and inv:is_empty("output") then
+				and inv:is_empty("output") and inv:is_empty("flux") then
 				return true
 			end
 			return false
 		end,
 		on_construct = function(pos)
-			local meta = minetest.get_meta(pos)
+			local meta = minetest.env:get_meta(pos)
 			meta:set_string("formspec", "size[8,7]"..
-			        "image[6,0;2,2;anvil_"..anvil[1].."_side.png]"..
-					"button[0.5,0.25;1.35,0.5;buttonForge;Forge]"..
-					"button[1.6,0.25;0.9,0.5;buttonForge10;x10]"..
+					"button[0.5,0.25;1.35,1;buttonForge;Forge]"..
+					"button[1.6,0.25;0.9,1;buttonForge10;x10]"..
 					"list[current_name;src1;2.9,0.25;1,1;]"..
 					"image[3.69,0.22;0.54,1.5;anvil_arrow.png]"..
 					"list[current_name;src2;4.1,0.25;1,1;]"..
-					"list[current_name;hammer;1,1;1,1;]"..
+					"button[5.5,0.25;1.35,1;buttonWeld;Weld]"..
+					"button[6.6,0.25;0.9,1;buttonWeld10;x10]"..
+					"list[current_name;hammer;1,1.5;1,1;]"..
 					"list[current_name;output;3.5,1.5;1,1;]"..
+					"list[current_name;flux;6,1.5;1,1;]"..
 					"list[current_player;main;0,3;8,4;]")
 			meta:set_string("infotext", anvil[2].." Anvil")
 			local inv = meta:get_inventory()
@@ -217,7 +220,7 @@ for _, anvil in ipairs(anvils) do
 			inv:set_size("flux", 1)
 		end,
 		on_receive_fields = function(pos, formname, fields, sender)
-			local meta = minetest.get_meta(pos)
+			local meta = minetest.env:get_meta(pos)
 			local inv = meta:get_inventory()
 	
 			local src1, src2 = inv:get_stack("src1", 1), inv:get_stack("src2", 1)
@@ -247,14 +250,47 @@ for _, anvil in ipairs(anvils) do
 					end
 				end
 			end
+			local weld = function()
+				if flux:get_name() == "minerals:flux" then
+					for _, recipe in ipairs(realtest.registered_anvil_recipes) do
+						if recipe.type == "weld" and recipe.item1 == src1:get_name() and recipe.item2 == src2:get_name() and
+							anvil[3] >= recipe.level and
+							minetest.get_item_group(instrument:get_name(),  recipe.instrument) == 1 and
+							minetest.get_item_group(instrument:get_name(), "material_level") >= recipe.level then
+							if inv:room_for_item("output", recipe.output) then
+								if recipe.rmitem1 then
+									src1:take_item()
+									inv:set_stack("src1", 1, src1)
+								end
+								if recipe.item2 ~= "" and recipe.rmitem2 then
+									src2:take_item()
+									inv:set_stack("src2", 1, src2)
+								end
+								output:add_item(recipe.output)
+								inv:set_stack("output", 1, output)
+								flux:take_item()
+								inv:set_stack("flux", 1, flux)
+								instrument:add_wear(65535/minetest.get_item_group(instrument:get_name(), "durability")/2)
+								inv:set_stack("hammer", 1, instrument)
+							end
+							return
+						end
+					end 
+				end
+			end
 			if fields["buttonForge"] then
 				forge()
 			elseif fields["buttonForge10"] then
 				for i = 0, 9 do
 					forge()
 				end
+			elseif fields["buttonWeld"] then
+				weld()
+			elseif fields["buttonWeld10"] then
+				for i = 0, 9 do
+					weld()
+				end
 			end
 		end,
 	})
 end
-
