72a54a7f949a582627201bee86d5e23b08730810
DanDuncombe <fishermanduncombe@gmail.com>
2013-11-14 19:09:48 +0000

    Improved Spikes

--- mods/spikes/init.lua
+++ mods/spikes/init.lua
@@ -1,5 +1,30 @@
 spikes = {}
 
+spikes.intllib_modpath = minetest.get_modpath("intllib")
+
+if minetest.get_modpath("unified_inventory") or not minetest.setting_getbool("creative_mode") then
+	spikes.expect_infinite_stacks = false
+else
+	spikes.expect_infinite_stacks = true
+end
+
+spikes.modpath = minetest.get_modpath("spikes")
+
+-- protection wrapper for 6d stuff
+
+function spikes.protect_and_rotate(itemstack, placer, pointed_thing)
+	if not spikes.node_is_owned(pointed_thing.under, placer) 
+	   and not spikes.node_is_owned(pointed_thing.above, placer) then
+		minetest.rotate_and_place(itemstack, placer, pointed_thing,
+			spikes.expect_infinite_stacks)
+	end
+	return itemstack
+end
+
+-- other components
+
+dofile(spikes.modpath.."/ownership.lua")
+
 for i, tree_name in ipairs(realtest.registered_trees_list) do
     local tree = realtest.registered_trees[tree_name]
 
@@ -31,6 +56,7 @@ for i, tree_name in ipairs(realtest.registered_trees_list) do
 	    walkable = false,
 	    damage_per_second = 10,
 	    fall_damage_add_percent = 200,
+	    on_place = spikes.protect_and_rotate
     }
     
     minetest.register_node("spikes:spike_"..tree.name:remove_modname_prefix(), spikes)
